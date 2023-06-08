331e801bae387622660229736747702c14e2c5de
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-06-08 21:55:06 +0200

    Made reusable equipment card and added to shop and home.
    Shop screen cards need to be nested in order to display cost and buy button below card.

--- src/screens/shopScreen.js
+++ src/screens/shopScreen.js
@@ -2,8 +2,9 @@
 import * as React from 'react';
 import { API_URL } from 'utils/urls';
 import { useSelector, useDispatch } from 'react-redux';
-import { Card, CardActions, CardContent, CardMedia, Typography, Button, createTheme, ThemeProvider } from '@mui/material';
+import { Card, CardActions, Typography, Button, createTheme, ThemeProvider } from '@mui/material';
 import { ShopWrapper, ShopTopDiv, ShopBotDiv, ShopImage, WaresWrapper } from 'components/shop/ShopScreenCSS';
+import { EquipmentCard } from 'components/equipments/EquipmentCard';
 import user from '../reducers/user';
 import goldIconIMG from '../assets/images/UI/coin.png'
 /* import buttonbackgroundIMG from '../assets/images/UI/buttonsmall.png' */
@@ -61,69 +62,49 @@ export const ShopScreen = () => {
           <WaresWrapper>
             {equipmentData.slice(1).map((singleWeapon) => {
               return (
-                <Card
-                  key={singleWeapon._id}
-                  sx={{
-                    width: '25vw',
-                    maxWidth: 175,
-                    minWidth: '145px',
-                    height: '97%',
-                    backgroundColor: '#edd99b',
-                    borderStyle: 'solid',
-                    borderColor: '#3b241c',
-                    scrollSnapAlign: 'start',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between'
-                  }}>
-                  <CardMedia
-                    sx={{ height: 85, backgroundSize: '55px' }}
-                    image={singleWeapon.img_src}
-                    title="weapon name" />
-                  <CardContent sx={{ padding: 1 }}>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      sx={{
-                        fontWeight: 900,
-                        fontSize: '1.2rem',
-                        lineHeight: 1
-                      }}>
-                      {singleWeapon.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ fontSize: '0.85rem', fontWeight: '700' }}>
-                      Damage: {singleWeapon.damage}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ fontSize: '0.75rem' }}>
-                      {singleWeapon.description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions sx={{ justifyContent: 'space-between' }}>
-                    <Typography sx={{ fontSize: '0.7rem', display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '55%' }}>
-                      {singleWeapon.cost}
-                      <img src={`${goldIconIMG}`} alt="gold display icon" />
-                    </Typography>
-                    <Button
-                      sx={{
+                <>
+                  <EquipmentCard singleWeapon={singleWeapon} />
+                  <Card
+                    key={singleWeapon._id}
+                    sx={{
+                      width: '25vw',
+                      maxWidth: 175,
+                      minWidth: '145px',
+                      height: '97%',
+                      backgroundColor: '#edd99b',
+                      borderStyle: 'solid',
+                      borderColor: '#3b241c',
+                      scrollSnapAlign: 'start',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'space-between'
+                    }}>
+                    <CardActions sx={{ justifyContent: 'space-between' }}>
+                      <Typography sx={{ fontSize: '0.7rem', display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '55%' }}>
+                        {singleWeapon.cost}
+                        <img src={`${goldIconIMG}`} alt="gold display icon" />
+                      </Typography>
+                      <Button
+                        sx={{
                         /* backgroundImage: `url(${buttonbackgroundIMG})`,
                           backgroundRepeat: 'no-repeat',
                           backgroundSize: 'cover', */
-                        color: 'white',
-                        fontWeight: 700,
-                        textDecoration: 'none',
-                        mt: 3,
-                        mb: 2,
-                        fontSize: '1.2rem',
-                        backgroundColor: '#3d4362',
-                        borderRadius: '14%'
-                      }}
-                      size="small"
-                      onClick={() => onBuyClick(singleWeapon._id)}>
+                          color: 'white',
+                          fontWeight: 700,
+                          textDecoration: 'none',
+                          mt: 3,
+                          mb: 2,
+                          fontSize: '1.2rem',
+                          backgroundColor: '#3d4362',
+                          borderRadius: '14%'
+                        }}
+                        size="small"
+                        onClick={() => onBuyClick(singleWeapon._id)}>
                       Buy
-                    </Button>
-                  </CardActions>
-                </Card>
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </>
               )
             })}
           </WaresWrapper>
